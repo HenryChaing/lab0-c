b267f664638cc9d9b0909990c54b1c4080cc47d4
HenryChaing <n96121210@gs.ncku.edu.tw>
2024-02-23 16:03:04 +0800

    Adapt Linux merge sort to q_sort()
    The Linux kernel utilizes a bottom-up approach to implement merge sort, a well-known method for caching. Drawing inspiration from this, I've incorporated the same technique into my q_sort() function. In order to implement q_sort(), several additional functions are required to support it, such as merge(), merge_final(), and compare().

--- queue.c
+++ queue.c
@@ -11,6 +11,11 @@
  */
 
 typedef struct list_head list_head;
+static list_head *merge(struct list_head *a, struct list_head *b);
+static void merge_final(struct list_head *head,
+                        struct list_head *a,
+                        struct list_head *b);
+static bool compare(struct list_head *a, struct list_head *b);
 
 /* Create an empty queue */
 list_head *q_new()
@@ -209,7 +214,77 @@ void q_reverseK(struct list_head *head, int k)
 }
 
 /* Sort elements of queue in ascending/descending order */
-void q_sort(struct list_head *head, bool descend) {}
+void q_sort(struct list_head *head, bool descend)
+{
+    struct list_head *list = head->next, *pending = NULL;
+    size_t count = 0; /* Count of pending */
+
+    if (list == head->prev) /* Zero or one elements */
+        return;
+
+    /* Convert to a null-terminated singly-linked list. */
+    head->prev->next = NULL;
+
+    /*
+     * Data structure invariants:
+     * - All lists are singly linked and null-terminated; prev
+     *   pointers are not maintained.
+     * - pending is a prev-linked "list of lists" of sorted
+     *   sublists awaiting further merging.
+     * - Each of the sorted sublists is power-of-two in size.
+     * - Sublists are sorted by size and age, smallest & newest at front.
+     * - There are zero to two sublists of each size.
+     * - A pair of pending sublists are merged as soon as the number
+     *   of following pending elements equals their size (i.e.
+     *   each time count reaches an odd multiple of that size).
+     *   That ensures each later final merge will be at worst 2:1.
+     * - Each round consists of:
+     *   - Merging the two sublists selected by the highest bit
+     *     which flips when count is incremented, and
+     *   - Adding an element from the input as a size-1 sublist.
+     */
+    do {
+        size_t bits;
+        struct list_head **tail = &pending;
+
+        /* Find the least-significant clear bit in count */
+        for (bits = count; bits & 1; bits >>= 1)
+            tail = &(*tail)->prev;
+        /* Do the indicated merge */
+        if (bits) {
+            struct list_head *a = *tail, *b = a->prev;
+
+            a = merge(b, a);
+            /* Install the merged result in place of the inputs */
+            a->prev = b->prev;
+            *tail = a;
+        }
+
+        /* Move one element from input list to pending */
+        list->prev = pending;
+        pending = list;
+        list = list->next;
+        pending->next = NULL;
+        count++;
+    } while (list);
+
+    /* End of input; merge together all the pending lists. */
+    list = pending;
+    pending = pending->prev;
+    for (;;) {
+        struct list_head *next = pending->prev;
+
+        if (!next)
+            break;
+        list = merge(pending, list);
+        pending = next;
+    }
+    /* The final merge, rebuilding prev links */
+    merge_final(head, pending, list);
+    if (!descend) {
+        q_reverse(head);
+    }
+}
 
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
@@ -233,4 +308,92 @@ int q_merge(struct list_head *head, bool descend)
 {
     // https://leetcode.com/problems/merge-k-sorted-lists/
     return 0;
+}
+
+
+static list_head *merge(struct list_head *a, struct list_head *b)
+{
+    struct list_head *head = NULL, **tail = &head;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (compare(a, b)) {
+            *tail = a;
+            tail = &a->next;
+            a = a->next;
+            if (!a) {
+                *tail = b;
+                break;
+            }
+        } else {
+            *tail = b;
+            tail = &b->next;
+            b = b->next;
+            if (!b) {
+                *tail = a;
+                break;
+            }
+        }
+    }
+    return head;
+}
+
+static void merge_final(struct list_head *head,
+                        struct list_head *a,
+                        struct list_head *b)
+{
+    struct list_head *tail = head;
+    int count = 0;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (compare(a, b)) {
+            tail->next = a;
+            a->prev = tail;
+            tail = a;
+            a = a->next;
+            if (!a)
+                break;
+        } else {
+            tail->next = b;
+            b->prev = tail;
+            tail = b;
+            b = b->next;
+            if (!b) {
+                b = a;
+                break;
+            }
+        }
+    }
+
+    /* Finish linking remainder of list b on to tail */
+    tail->next = b;
+    do {
+        /*
+         * If the merge is highly unbalanced (e.g. the input is
+         * already sorted), this loop may run many iterations.
+         * Continue callbacks to the client even though no
+         * element comparison is needed, so the client's cmp()
+         * routine can invoke cond_resched() periodically.
+         */
+        if (!++count)
+            compare(a, b);
+        b->prev = tail;
+        tail = b;
+        b = b->next;
+    } while (b);
+
+    /* And the final links to make a circular doubly-linked list */
+    tail->next = head;
+    head->prev = tail;
+}
+
+static bool compare(struct list_head *a, struct list_head *b)
+{
+    element_t *nodeA = container_of(a, element_t, list);
+    element_t *nodeB = container_of(b, element_t, list);
+    if (strcmp(nodeA->value, nodeB->value) <= 0) {
+        return false;
+    }
+    return true;
 }
